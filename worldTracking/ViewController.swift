70d48849a3bd2b8df940f4c533cf505619412db4
mauhdz <https://github.com/mauhdz/>
2017-11-28 16:53:44 +1300

    Initial Commit

---
+++ worldTracking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  worldTracking
+//
+//  Created by Mac User on 28/11/17.
+//  Copyright © 2017 Mac User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
